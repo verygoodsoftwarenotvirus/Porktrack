7756814e77b1c608c7497691406bf60e88debb08
Jeffrey D. <literallyelvis@gmail.com>
2014-07-29 19:13:54 -0500

    Update youtube_get.py
    Uses .format() instead of concatenation. Also minor style changes.

--- scripts/youtube_get.py
+++ scripts/youtube_get.py
@@ -1,13 +1,10 @@
-__author__ = "Jeffrey D."
-__copyright__ = "don't sweat it"
-__license__ = "nah"
-__version__ = "0.4"
-__email__ = "literallyelvis" + "@" + "gmail.com"
+__author__ = 'Jeffrey D.'
+__version__ = '0.4'
 
 from urllib.request import urlopen
 
 
-def retrieveLink(url):
+def retrieve_link(url):
     """Returns HTML code for a particular url"""
     retrieve = urlopen(url)
     read = retrieve.read()
@@ -15,22 +12,19 @@ def retrieveLink(url):
     return html
 
 
-def retrieveVideoID(query):
+def retrieve_video_id(query):
     """Returns the YouTube video ID of the top search result for a query."""
     query = query.replace('\n', '')
-    query = query.replace(" ", "+")
-    query = query.replace("&", "and")
-
-    video_url = 'https://www.youtube.com/results?search_query=' + query
+    query = query.replace(' ', '+')
+    query = query.replace('&', 'and')
+    video_url = 'https://www.youtube.com/results?search_query={0}'.format(query)
     youtube_result = retrieveLink(video_url)
-
     begin = youtube_result.find('<ol id="search-results"') + 23
     begin = youtube_result.find('a href="', begin) + 8
     end = youtube_result.find('" class="', begin)
     begin = youtube_result.find('watch?v=', begin) + 8
-
     video_id = youtube_result[begin:end]
-    if "&amp;list=" in video_id:  # handles when search returns a YT list.
-        mark = video_id.find("&amp;list=")
+    if '&amp;list=' in video_id:  # handles when search returns a YT list.
+        mark = video_id.find('&amp;list=')
         video_id = video_id[:mark]
     return video_id
