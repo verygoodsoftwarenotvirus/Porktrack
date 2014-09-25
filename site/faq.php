095e96cc4bc84749c61e2f1852989aa68705534e
Jeffrey D <literallyelvis@gmail.com>
2014-09-24 20:51:24 -0500

    Cleaned up code.

--- site/faq.php
+++ site/faq.php
@@ -16,20 +16,19 @@
 		margin-top: 10px;
 		margin-left: auto;
 		margin-right: auto;
-		padding: 10px;
-	}
+		padding: 10px; }
+
 	.ans { 
 		display:none;
 		margin-top:7px;
-		text-align: left;
-	}
+		text-align: left; }
+
 	</style>
+
 	<script type="text/javascript">
 	function toggle(Info) {
 	var CState = document.getElementById(Info);
-	CState.style.display = (CState.style.display != 'block')
-						? 'block' : 'none';
-	}
+	CState.style.display = (CState.style.display != 'block') ? 'block' : 'none'; }
 	</script>
 </head>
 
@@ -39,21 +38,29 @@
 	<div class="faq" onclick="toggle('faq1')">
 		What the heck is a porktrack?
 		<div id="faq1" class="ans"><hr>
-			Simply put, it is the <a href="http://www.billboard.com/charts/hot-100">Billboard Hot 100</a> song for the week of your estimated conception. The idea is that if any song, in the indescribably vast collection of songs human beings have made in our years as a creative species, is likely to be the song your parents <em>did the deed</em> &nbsp to, it's the #1 song in the country at that particular time.
+			Simply put, it is the <a href="http://www.billboard.com/charts/hot-100">Billboard Hot 100</a> 
+			song for the week of your estimated conception. The idea is that if any song, in the indescribably 
+			vast collection of songs human beings have made in our years as a creative species, is likely to 
+			be the song your parents <em>did the deed</em> &nbsp to, it's the #1 song in the country at that 
+			particular time.
 		</div>
 	</div>
 
 	<div class="faq" onclick="toggle('faq2')">
 		How accurate is it?
 		<div id="faq2" class="ans"><hr>
-			Not very. The site takes your submitted birthdate, subtracts 40 weeks, then adds or subtracts whatever offset you put in if you check the peculiar birth box. Real doctors can only kind of estimate the date of conception, and they have real, tangible data to work with. This is just a computer somewhere.
+			Not very. The site takes your submitted birthdate, subtracts 40 weeks, then adds or subtracts 
+			whatever offset you put in if you check the peculiar birth box. Real doctors can only kind of 
+			estimate the date of conception, and they have real, tangible data to work with. This is just 
+			a computer somewhere.
 		</div>
 	</div>
 
 	<div class="faq" onclick="toggle('faq3')">
 		Why make this?
 		<div id="faq3" class="ans"><hr>
-			I really wanted to learn how to make a website. Also it amuses me greatly to see how people react to their porktracks.
+			I really wanted to learn how to make a website. Also it amuses me greatly to see how people react
+			to their porktracks.
 		</div>
 	</div> 
 
--- site/index.php
+++ site/index.php
@@ -1,11 +1,11 @@
 <html>  
-	<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+	<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon"/>
 	<link type="text/css" rel="stylesheet" href="stylesheet.css"/>
 	<link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
 	<head>
 		<title>Porktrack</title>		
 		<meta property="og:site_name" content="Porktrack">
-		<meta property="og:url" content="http://www.porktrack.com".>
+		<meta property="og:url" content="http://www.porktrack.com">
 		<meta property="og:title" content="Porktrack">
 		<meta property="og:image" content="http://www.porktrack.com/images/avatar.png">
 		<meta property="og:description" content="What is your porktrack?">	
@@ -48,61 +48,57 @@
               newOption.innerHTML = i.toString();
               year_select.options.add(newOption);}}         
 		</script>
-		<form 
-		   name="special" 
-		   method="GET" 
-		   action="results.php">
-        <div class="birthday">
-        Choose a list:
-        <select name="list" id="list" onchange="populate(this.id, 'year');ShowPart('button')">
-            <option value="" selected></option>
-            <option value="track">Hot 100</option>
-            <option value="country">Country</option>
-            <option value="latin">Latin</option>
-        </select>
-        
-			<br><br>Enter your birthdate:<br> 
-			<select name="year" id="year"></select>
-			<select name="month">
-				<option value="01" >January</option>
-				<option value="02" >February</option>
-				<option value="03" >March</option>
-				<option value="04" >April</option>
-				<option value="05" >May</option>
-				<option value="06" >June</option>
-				<option value="07" >July</option>
-				<option value="08" >August</option>
-				<option value="09" >September</option>
-				<option value="10" >October</option>
-				<option value="11" >November</option>
-				<option value="12" >December</option>
-			</select>
-			<select name="day">
-				<option>01</option><option>02</option><option>03</option>
-				<option>04</option><option>05</option><option>06</option>
-                <option>07</option><option>08</option><option>09</option>
-				<option>10</option><option>11</option><option>12</option>
-				<option>13</option><option>14</option><option>15</option>
-				<option>16</option><option>17</option><option>18</option>
-				<option>19</option><option>20</option><option>21</option>
-				<option>22</option><option>23</option><option>24</option>
-				<option>25</option><option>26</option><option>27</option>
-				<option>28</option><option>29</option><option>30</option>
-				<option>31</option>
-			</select>
-		</div>		
+		
+		<form method="GET" action="results.php">
+			<div class="birthday">
+		        Choose a list:
+		        <select id="list" onchange="populate(this.id, 'year');ShowPart('button')">
+		            <option value="" selected></option>
+		            <option value="track">Hot 100</option>
+		            <option value="country">Country</option>
+		            <option value="latin">Latin</option>
+		        </select>	        
+					<br><br>Enter your birthdate:<br> 
+					<select name="year" id="year"></select>
+					<select name="month">
+						<option value="01" >January</option>
+						<option value="02" >February</option>
+						<option value="03" >March</option>
+						<option value="04" >April</option>
+						<option value="05" >May</option>
+						<option value="06" >June</option>
+						<option value="07" >July</option>
+						<option value="08" >August</option>
+						<option value="09" >September</option>
+						<option value="10" >October</option>
+						<option value="11" >November</option>
+						<option value="12" >December</option>
+					</select>
+					<select name="day">
+						<option>01</option><option>02</option><option>03</option>
+						<option>04</option><option>05</option><option>06</option>
+		                <option>07</option><option>08</option><option>09</option>
+						<option>10</option><option>11</option><option>12</option>
+						<option>13</option><option>14</option><option>15</option>
+						<option>16</option><option>17</option><option>18</option>
+						<option>19</option><option>20</option><option>21</option>
+						<option>22</option><option>23</option><option>24</option>
+						<option>25</option><option>26</option><option>27</option>
+						<option>28</option><option>29</option><option>30</option>
+						<option>31</option>
+					</select>
+			</div>		
      
-		<br><br><br><p class="special" id="specialCheck"><br><br>
-			<input 
-				type="checkbox" 
-				value="no" 
-				onclick="CheckboxChecked(this.checked,'reveal')">
+		<br><br><br>
+		<p id="specialCheck"><br><br>
+			<input type="checkbox" value="no" onclick="CheckboxChecked(this.checked,'reveal')">
 			I had a peculiar birth!
 		</p>		
-		<div class="special" id="reveal" style="display:none">
+
+		<div id="reveal" style="display:none">
 			<p>
 				I was born
-				<input type="number" min="0" value="0" max="365" class="birthday" name="offset" id="offset">
+				<input type="number" min="0" value="0" max="365" class="birthday" id="offset">
 				<select name="timetype">
 					<option>days</option>
 					<option>weeks</option>
@@ -115,8 +111,7 @@
 		</div>		
         
 		<div class="submit">
-            <input type="submit" id='button' class="btn-style" value="Let's Pork!" style="display:none" /><br><br>
-			
+            <input type="submit" id='button' class="btn-style" value="Let's Pork!" style="display:none"/><br><br>
             <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 			<!-- bottom_main -->
 			<ins class="adsbygoogle"
@@ -126,12 +121,11 @@
 			<script>
 			(adsbygoogle = window.adsbygoogle || []).push({});
 			</script>
-            
-		</div>
+        </div>
 		</form>
 	
 		<script type="text/javascript">
-			CheckboxChecked(document.myform.snowflake.checked,'reveal');
+			CheckboxChecked(document.myform.checked,'reveal');
 		</script>
 	</div>
 	
@@ -139,5 +133,6 @@
 		<a href="http://www.porktrack.com/faq.php">FAQ</a> &#8226 
 		<a href="http://www.twitter.com/porktrack">follow @porktrack</a> &#8226 
 		<a href="http://www.twitter.com/literallyelvis">created by</a>
-		<a href="https://github.com/LiterallyElvis/Porktrack"><img style="position: absolute; bottom: 0; right: 0; border: 0;" src="images/forkme.png" alt="Pork me on GitHub" data-canonical-src="images/forkme.png"></a>
+		<a href="https://github.com/LiterallyElvis/Porktrack">
+		<img style="position: absolute; bottom: 0; right: 0; border: 0;" src="images/forkme.png" alt="Pork me on GitHub" data-canonical-src="images/forkme.png"></a>
 	</div>
